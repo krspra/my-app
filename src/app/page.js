d951b44f5787d223b1d8cfbe003f5842e014f23e
Pradeep Yadav <pradeepyd708@gmail.com>
2025-03-19 15:16:13 +0530

    added some more code

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,38 @@
 import { useState } from "react";
 
 export default function Home() {
-  const formData=useState([]);
+  const formData=useState(["text"]);
+
+  const handleForm=(e)=>{
+    let choice=e.target.innerText;
+    if(choice=="Text"){
+      formData.push("text")
+    }
+    else if(choice=="Number"){
+      formData.push("number")
+    }
+    else if(choice=="Date"){
+      formData.push("date")
+    }
+    else if(choice=="Checkbox"){
+      formData.push("checkbox")
+    }
+  }
   return (
     <>
       <div className="w-full text-center text-5xl">Avlokan</div>
-      <div className="w-full flex justify-around mt-4">
-        <button className="cursor-pointer bg-amber-500 p-2 rounded-2xl w-[80px]">Text</button>
-        <button className="cursor-pointer bg-pink-500 p-2 rounded-2xl w-[80px]">Number</button>
-        <button className="cursor-pointer bg-green-500 p-2 rounded-2xl w-[80px]">Date</button>
-        <button className="cursor-pointer bg-red-500 p-2 rounded-2xl w-[90px]">Checkbox</button>
+      <div className="w-full flex justify-around mt-4" onClick={handleForm
+      }>
+        <button className="cursor-pointer bg-amber-500 p-2 rounded-2xl w-[80px] text-white">Text</button>
+        <button className="cursor-pointer bg-pink-500 p-2 rounded-2xl w-[80px] text-white">Number</button>
+        <button className="cursor-pointer bg-green-500 p-2 rounded-2xl w-[80px] text-white">Date</button>
+        <button className="cursor-pointer bg-red-500 p-2 rounded-2xl w-[90px] text-white">Checkbox</button>
       </div>
-
       <form>
-        
+        {
+          formData.map((val)=>console.log(val)
+          )
+        }
       </form>
     </>
   );
